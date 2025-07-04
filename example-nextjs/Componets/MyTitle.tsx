3d9c646c870ef8d9dc6becb303e57cd7777db87b
DANIEL CAZABAT <dacazabat@gmail.com>
2025-07-04 09:03:42 -0300

    Add example in NextJS (ReactJS Front)

---
+++ example-nextjs/Componets/MyTitle.tsx
@@ -0,0 +1,7 @@
+export default function MyTitle() {
+    return(
+        <h1 className="text-4xl font-bold text-center m-8">
+            Example App in NextJS
+        </h1>
+    )
+}
---
+++ example-nextjs/app/about/page.tsx
@@ -0,0 +1,12 @@
+import MyTitle from "@/Componets/MyTitle";
+
+export default function About() {
+    return (
+        <>
+            <MyTitle />
+            <div className="flex h-screen items-center justify-center">
+                <h1 className="text-4xl font-bold">About Page</h1>
+            </div>
+        </>
+    )
+}   
